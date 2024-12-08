3de2d9ee23b51697ed024da07b72cf8473c59ab6
TriNitki <115486555+TriNitki@users.noreply.github.com>
2024-12-09 02:40:25 +0700

    GIGA ABOBUS

--- lab4/main.py
+++ lab4/main.py
@@ -1,8 +1,9 @@
-from methods import gradient_descent
+from methods import gradient_descent,  cauchy_method
 
 def main():
     func = "x**3+y**3-15*x*y"
-    print(gradient_descent(func, [5.23, 4.41]))
+    print(gradient_descent(func, [5.23, 5.41]))
+    print(cauchy_method(func, [5.23, 5.41]))
 
 if __name__ == "__main__":
     main()
--- lab4/methods.py
+++ lab4/methods.py
@@ -1,3 +1,4 @@
+from math import sqrt
 from sympy import diff
 
 def _f(func: str, value1: float, value2: float, symbol1: str = "x", symbol2: str = "y") -> float:
@@ -10,20 +11,53 @@ def _g(func):
     grad = [_d(func, "x"), _d(func, "y")]  # Частные производные
     return grad
 
-def gradient_descent(func, x_start, learning_rate=0.01, tol=1e-4, max_iter=1000):
-    x = x_start[:]
+def norm(vector):
+    return sqrt(sum(v**2 for v in vector))
+
+def vector_add(v1, v2, alpha=1.0):
+    return [v1[i] + alpha * v2[i] for i in range(len(v1))]
+
+def find_lambda(func, x, grad, lambda_min=0, lambda_max=1, tol=1e-4):
+    while lambda_max - lambda_min > tol:
+        mid1 = lambda_min + (lambda_max - lambda_min) / 3
+        mid2 = lambda_max - (lambda_max - lambda_min) / 3
+        f1 = _f(func, *vector_add(x, grad, alpha=-mid1))
+        f2 = _f(func, *vector_add(x, grad, alpha=-mid2))
+        if f1 < f2:
+            lambda_max = mid2
+        else:
+            lambda_min = mid1
+    return (lambda_min + lambda_max) / 2
+
+def gradient_descent(func, x0, learning_rate=0.03, tol=1e-4, max_iter=1000):
+    x = x0[:]
     num_iterations = 0
 
-    while num_iterations < max_iter:
+    for i in range(max_iter):
         grad = _g(func)
         grad_values = [_f(grad[0], *x), _f(grad[1], *x)]
         x_new = [x[i] - learning_rate * grad_values[i] for i in range(len(x))]
 
-        # Остановка, если разница между новыми и старыми значениями достаточно мала
-        if all(abs(x_new[i] - x[i]) < tol for i in range(len(x))):
+        if norm(grad_values) < tol:
             break
 
         x = x_new
         num_iterations += 1
 
-    return x
+    return x, i
+
+# Метод Коши
+def cauchy_method(func, x0, tol=1e-4, max_iter=100):
+    x = x0[:]
+    for i in range(max_iter):
+        grad = _g(func)
+        grad_values = [_f(grad[0], *x), _f(grad[1], *x)]
+        
+        if norm(grad_values) < tol:
+            break
+        
+        _lambda = find_lambda(func, x, grad_values)
+        
+        x = vector_add(x, grad_values, alpha=-_lambda)
+    
+    return x, i
